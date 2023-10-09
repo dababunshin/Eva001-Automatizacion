2ab620b949d1cc3a9cab573686efa1b9ac07a43c
dababunshin <121452349+dababunshin@users.noreply.github.com>
2023-10-09 17:41:01 -0300

    Add files via upload

---
+++ src/main/java/cl/iplacex/eva001dbarria/MainApp.java
@@ -0,0 +1,9 @@
+package cl.iplacex.eva001dbarria;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("EVA 001 - DANIEL BARRIA");
+        System.out.println("CASO: RETAIL OFF");
+        System.out.println("ING. INFORMATIVA 2023");
+    }
+}
